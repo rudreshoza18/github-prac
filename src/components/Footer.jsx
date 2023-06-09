6eedf39050ca74c9a9837905d7365b0df265b4ef
Rudresh Oza <ozarudresh98@gmail.com>
2023-06-09 21:55:47 +0530

    updated header component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const Footer = () => {
+  return (
+    <div>
+      <nav>
+        <ol>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ol>
+      </nav>
+    </div>
+  );
+};
