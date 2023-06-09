e54712309e582ffbf384eefca0f6402f13939c9f
Rudresh Oza <ozarudresh98@gmail.com>
2023-06-09 21:39:06 +0530

    changes in app.js added some text

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
 
 function App() {
-  return <div>HELLO GITHUB</div>;
+  return (
+    <div>
+      HELLO GITHUB Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+      Facere, dolor totam! Et nesciunt pariatur deleniti fugiat, assumenda
+      exercitationem optio doloremque quod minus saepe, voluptas hic suscipit
+      necessitatibus corrupti! Perspiciatis, inventore!
+    </div>
+  );
 }
 
 export default App;
